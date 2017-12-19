246035048acec4d54eb35fe149287611bf568bdf
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-12-19 00:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your solution in this file!
 var customerName = "bob";
+
 function upperCaseCustomerName(){
 
- var upper = customerName.toLowerCase();
- return upper;
+     return customerName.toUpperCase();
+
 }
